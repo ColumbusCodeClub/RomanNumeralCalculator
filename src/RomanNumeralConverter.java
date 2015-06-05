1ae6b17965dfad2d418b18d2394dc508859e002e
blseich <blseich@gmail.com>
2015-06-05 13:03:45 -0400

    feat: Refine recursive strategy. Support multiple two numeral digits

--- src/RomanNumeralConverter.java
+++ src/RomanNumeralConverter.java
@@ -14,21 +14,46 @@ public class RomanNumeralConverter {
 	}
 	
 	public Integer convert(String romanNumeral) {
-		String firstDigit = romanNumeral.substring(0, 1);
-		String endDigits = romanNumeral.substring(1);
 		
-		if(romanNumeral.length() != 1) {
-			String nextDigit = endDigits.substring(0, 1);
-			int digitDifference = lookUpDigit(firstDigit) - lookUpDigit(nextDigit);
-			if (digitDifference < 0) {
-				return Math.abs(digitDifference);
-			}
-			return lookUpDigit(firstDigit) + convert(endDigits);
-		} else {
-			return lookUpDigit(firstDigit);
-		}
+//		String firstDigit = romanNumeral.substring(0, 1);
+//		String endDigits = romanNumeral.substring(1);
+		
+		
+		
+		return arabicNumeralBuilder(0, 0, romanNumeral);
+//		
+//		if(romanNumeral.length() == 1)
+//			return lookUpDigit(firstDigit);
+//		
+//		String nextDigit = endDigits.substring(0, 1);
+//		int digitDifference = lookUpDigit(firstDigit) - lookUpDigit(nextDigit);
+//		if (digitDifference < 0) {
+//			if (endDigits.length() > 1) {
+//			return Math.abs(digitDifference)
+//					+ convert(endDigits.substring(1));
+//			} else {
+//				return Math.abs(digitDifference);
+//			}
+//		}
+//		return lookUpDigit(firstDigit) + convert(endDigits);
 	}
 	
+	private Integer arabicNumeralBuilder(int previousDigitValue, int totalDigitValue, String romanNumeral) {
+		if(romanNumeral.length() == 0)
+			return totalDigitValue;
+					
+		String digit = romanNumeral.substring(romanNumeral.length()-1);
+		String endDigits = romanNumeral.substring(0, romanNumeral.length()-1);
+		int digitValue = lookUpDigit(digit);
+
+		if (lookUpDigit(digit) < previousDigitValue) {
+			return arabicNumeralBuilder(digitValue, totalDigitValue-digitValue, endDigits);
+		} else {			
+			return arabicNumeralBuilder(digitValue, totalDigitValue+digitValue, endDigits);
+		}
+		
+	}
+
 	private Integer lookUpDigit(String digit) {
 		return romanNumbersMap.get(digit);
 	}
--- test/RomanNumeralConverterTest.java
+++ test/RomanNumeralConverterTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 import org.hamcrest.CoreMatchers;
 import org.hamcrest.MatcherAssert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 
@@ -53,6 +54,31 @@ public class RomanNumeralConverterTest {
 	@Test
 	public void shouldConvertIXto9() {
 		assertThat(underTest.convert("IX"), is(9));
+	}
+	
+	@Test
+	public void shouldConvertXIVto14(){
+		assertThat(underTest.convert("XIV"), is(14));
+	}
+	
+	@Test
+	public void shouldConvertXVto15(){
+		assertThat(underTest.convert("XV"), is(15));
+	}
+	
+	@Test
+	public void shouldConvertXIXto19(){
+		assertThat(underTest.convert("XIX"), is(19));
+	}
+	
+	@Test
+	public void shouldConvertXLto40(){
+		assertThat(underTest.convert("XL"), is(40));
+	}
+	
+	@Test
+	public void shouldConvertXLIVto44(){
+		assertThat(underTest.convert("XLIV"), is(44));
 	}
 	
 	@Test
